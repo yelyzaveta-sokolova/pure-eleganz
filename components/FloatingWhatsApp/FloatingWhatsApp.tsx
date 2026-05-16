68bef57969231ad168ae70a895ceee2064d1caf4
yelyzaveta-sokolova <sokolova.yelyzavetaaa@gmail.com>
2026-05-16 22:36:11 +0200

    fix mobile sugaring section

--- components/FloatingWhatsApp/FloatingWhatsApp.tsx
+++ components/FloatingWhatsApp/FloatingWhatsApp.tsx
@@ -3,7 +3,7 @@ import styles from "./FloatingWhatsApp.module.css";
 export default function FloatingWhatsApp() {
   return (
     <a
-      href="https://wa.me/380953659707"
+      href="https://wa.me/491757043089"
       target="_blank"
       className={styles.button}
     >
